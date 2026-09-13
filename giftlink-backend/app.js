5acbd47601d3ae0cb4eb6904d3755f29d6a5d026
Fareeha248 <fareehashahid864@gmail.com>
2026-09-13 12:34:39 -0400

    developed backend integration for registration

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,37 +1,40 @@
-/*jshint esversion: 8 */
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
+const { loadData } = require("./util/import-mongo/index");
 
-
-const app = express();
-app.use("*",cors());
-const port = 3060;
-
-// Connect to MongoDB; we just do this one time
-connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
-})
-    .catch((e) => console.error('Failed to connect to DB', e));
-
-
-app.use(express.json());
-
-// Route files
+const authRoutes = require('./routes/authRoutes');
 const giftRoutes = require('./routes/giftRoutes');
 const searchRoutes = require('./routes/searchRoutes');
+
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
+const app = express();
+const port = 3060;
+
+// Middleware
+app.use("*", cors());
+app.use(express.json());
 app.use(pinoHttp({ logger }));
 
-// Use Routes
+// Routes
+app.use('/api/auth', authRoutes);
 app.use('/api/gifts', giftRoutes);
 app.use('/api/search', searchRoutes);
+
+// Connect to MongoDB; we just do this one time
+connectToDatabase()
+    .then(() => {
+        pinoLogger.info('Connected to DB');
+    })
+    .catch((e) => {
+        console.error('Failed to connect to DB', e);
+    });
 
 // Global Error Handler
 app.use((err, req, res, next) => {
@@ -39,10 +42,13 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+// Root route
+app.get("/", (req, res) => {
+    res.send("Inside the server");
+});
 
+// Start server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
